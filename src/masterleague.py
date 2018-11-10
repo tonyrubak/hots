397dc1379e061dcf0b756cf0877a22ef69114a94
tonyrubak <joe@crimsonhexagon.us>
2018-11-10 14:00:59 -0500

    Changed get_heroes to return a list and added usage

--- src/masterleague.py
+++ src/masterleague.py
@@ -13,7 +13,7 @@ def get_maps():
     return response
 
 def get_heroes():
-    heroes = dict()
+    heroes = []
     total = 0
     page = 1
     num_heroes = 1
@@ -21,10 +21,14 @@ def get_heroes():
         response = json.loads(masterleague_get("heroes", options="&page=" + str(page)))
         num_heroes = response["count"]
         for hero in response["results"]:
-            heroes.update({hero["name"]: hero})        
+            heroes.append({hero["name"]: hero})        
         total += len(response["results"])
         page += 1
     return heroes
+
+def get_match(match_id):
+    response = json.loads(masterleague_get("matches/" + str(match_id)))
+    return response
 
 def heroes_to_csv(heroes):
     tab = str.maketrans('', '', string.punctuation)
@@ -33,11 +37,24 @@ def heroes_to_csv(heroes):
     df = df.set_index("id")[["name"]]
     df.to_csv("heroes.csv")
 
-def get_match(match_id):
-    response = json.loads(masterleague_get("matches/" + str(match_id)))
-    return response
+def heroes_to_df(heroes):
+    tab = str.maketrans('', '', string.punctuation)
+    df = pd.DataFrame(heroes)
+    df["name"] = df["name"].str.lower().str.translate(tab).str.replace("the ","").str.replace("lost ", "").str.replace(" ", "")
+    df = df.set_index("id")[["name"]]
+    return df
+    
+def heroes_to_dict(heroes):
+    tab = str.maketrans('', '', string.punctuation)
+    df = pd.DataFrame(heroes)
+    df["name"] = df["name"].str.lower().str.translate(tab).str.replace("the ","").str.replace("lost ", "").str.replace(" ", "")
+    df = df.set_index("id")[["name"]]
+    d = dict()
+    for (i,h) in df.iterrows():
+        d[i] = h["name"]
+    return d
 
-def match_to_draft(match, heroes):
+def match_to_draft(match, maps, heroes):
     first = match["drafts"][0]
     second =  match["drafts"][1]
     draft = []
@@ -59,4 +76,17 @@ def match_to_draft(match, heroes):
     draft.append(first["picks"][3]["hero"])
     draft.append(first["picks"][4]["hero"])
     draft.append(second["picks"][4]["hero"])
-    return [hero for hero in map(lambda x: heroes.loc[x], draft)]
+    m = maps[match["map"]]
+    heroes = [hero for hero in map(lambda x: heroes[x], draft)]
+    return [m, *heroes]
+
+# Usage
+heroes_dict = heroes_to_dict(heroes)
+m = get_match(7772)
+maps = pd.read_csv("maps")
+maps_dict = dict()
+for (idx, m) in maps.iterrows():
+    maps_dict[m["id"]] = m["name"]
+match_to_draft(m, maps_dict, heroes_dict)
+
+mlist = [7772, 
